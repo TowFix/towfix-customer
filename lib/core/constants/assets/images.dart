50aa1d0cd9a69a8868dcd17feb316305ad1fef8b
nana-agyeman1 <alexagyeman98@gmail.com>
2023-09-15 18:51:43 +0000

    registers more assets

--- lib/core/constants/assets/images.dart
+++ lib/core/constants/assets/images.dart
@@ -8,4 +8,11 @@ class Images {
   static const slide2 = 'assets/images/slide-2.png';
   static const slide3 = 'assets/images/slide-3.png';
   static const auth = 'assets/images/auth.png';
+  static const addVehicle = 'assets/images/undraw_Add_files_re_v09g 1.png';
+  static const standardTowTruck = 'assets/images/standard_tow_truck.png';
+  static const comfortableTowTruck = 'assets/images/comfortable_truck.png';
+  static const deluxeTowTruck = 'assets/images/deluxe_truck.png';
+  static const visa = 'assets/images/visa.png';
+  static const locationPicker = 'assets/images/towfix_location.png';
+  static final googleLogo = 'assets/images/google_logo.png';
 }
