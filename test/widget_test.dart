bf50d2abe97de278e8d89882be46904c51562c36
nana-agyeman1 <alexagyeman98@gmail.com>
2023-09-15 18:41:34 +0000

    add ui common package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:towfix/main.dart';
+import 'package:towfix/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
