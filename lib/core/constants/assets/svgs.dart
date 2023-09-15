b689ed763bbe43af26b578b695e4f4606c05c472
nana-agyeman1 <alexagyeman98@gmail.com>
2023-09-15 18:53:29 +0000

    more configs

---
+++ lib/core/constants/assets/svgs.dart
@@ -0,0 +1,6 @@
+class Svgs {
+  static const menuIcon = 'assets/svg/menu.svg';
+  static const towtruck = 'assets/svg/towtruck.svg';
+  static const mechanicfix = 'assets/svg/mechanicFix.svg';
+  static const mechanicshop = 'assets/svg/mechanicShop.svg';
+}
